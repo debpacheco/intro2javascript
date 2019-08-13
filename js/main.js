b69c23a1bd82b8658724708b92fc91308e7869cf
Debbie Pacheco <pachecodeb@yahoo.com>
2019-08-12 20:51:56 -0700

    day one javascript practice console log

---
+++ js/main.js
@@ -0,0 +1,15 @@
+let question;
+function myFunk() {
+    question = prompt ('Tell me your name.');
+    console.log('Your name is' + question);
+}
+
+console.log('I love thrifting');
+
+console.log('I like food');
+
+console.log('I like reading');
+
+console.log('I like naps');
+
+alert('Cats rule everything around me');
