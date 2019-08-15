1dd3f1dd6fdbbf68e67ab017a95e6e63df3cb238
Debbie Pacheco <pachecodeb@yahoo.com>
2019-08-14 19:52:59 -0700

    day one

---
+++ js/main.js
@@ -0,0 +1,27 @@
+//var firstName = 'Debbie';
+//var age = 30;
+
+//const firstName = 'Debbie';
+//console.log(firstName.length);
+
+//old way
+//console.log('My name is ' + firstName + ' and my age is' + age);
+
+//BETTER way
+//console.log(`My name is ${firstName} and my age is ${age}`);
+
+//alert(`Hello, I am ${firstName}`);
+
+//const birthday = '01/17/1989';
+//console.log(birthday);
+
+
+
+//let hello = new String('Hello, there.');
+//alert(hello);
+
+
+const topThreeMovies = ['Dodgeball', 'Fight Club', 'Grease'];
+console.log(topThreeMovies);
+console.log(`my second top movie is:${topThreeMovies[1]}`);
+
