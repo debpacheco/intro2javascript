a5cd756eaa0b9566e5de3c93330526d7d0e39639
Debbie Pacheco <pachecodeb@yahoo.com>
2019-08-21 18:15:34 -0700

    day 4

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const greeting = "hello";
+
+if (greeting === "hi") {
+    console.log('Yes, this matches to HI');
+
+} else if (greeting == "hello"){
+    console.log('Yes, this matched HELLO');
+}else if {
+    console.log('none of them matched');
+}
